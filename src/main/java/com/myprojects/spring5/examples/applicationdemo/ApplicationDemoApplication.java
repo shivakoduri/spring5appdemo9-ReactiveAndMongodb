77fbbbbbabf6518da73e9a4e81ca480f908bba9e
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-03-27 20:11:29 +0530

    Initial commit for Spring5 Recipe application demo

---
+++ src/main/java/com/myprojects/spring5/examples/applicationdemo/ApplicationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.spring5.examples.applicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myprojects/spring5/examples/applicationdemo/ApplicationDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myprojects.spring5.examples.applicationdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
