8a7777301a4aae6666103455f75688be0f0c376a
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-04-26 00:07:44 +0530

    Fix for stack overflow issue

--- src/main/java/com/myprojects/spring5/examples/domain/Notes.java
+++ src/main/java/com/myprojects/spring5/examples/domain/Notes.java
@@ -4,18 +4,19 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Data
-@Entity
-public class Notes {
+    @Data
+    @EqualsAndHashCode(exclude = {"recipe"})
+    @Entity
+    public class Notes {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @OneToOne
-    private Recipe recipe;
+        @OneToOne
+        private Recipe recipe;
 
-    @Lob
-    private String recipeNotes;
+        @Lob
+        private String recipeNotes;
 
 }
