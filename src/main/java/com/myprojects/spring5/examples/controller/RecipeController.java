3fb3f9fe155ea1b83e5834cfd1cd421b72cef399
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-03 21:26:36 +0530

    Commit for Creating a new Recipe

--- src/main/java/com/myprojects/spring5/examples/controller/RecipeController.java
+++ src/main/java/com/myprojects/spring5/examples/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package com.myprojects.spring5.examples.controller;
 
+import com.myprojects.spring5.examples.commands.RecipeCommand;
 import com.myprojects.spring5.examples.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
