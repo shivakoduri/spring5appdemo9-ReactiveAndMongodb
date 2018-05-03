3bc073520d77f59063c6acd18a212d52782f1e9d
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-03 23:24:30 +0530

    Commit for Updating Recipe

--- src/test/java/com/myprojects/spring5/examples/services/RecipeServiceImplTest.java
+++ src/test/java/com/myprojects/spring5/examples/services/RecipeServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.myprojects.spring5.examples.services;
 
+import com.myprojects.spring5.examples.commands.RecipeCommand;
 import com.myprojects.spring5.examples.converters.RecipeCommandToRecipe;
 import com.myprojects.spring5.examples.converters.RecipeToRecipeCommand;
 import com.myprojects.spring5.examples.domain.Recipe;
@@ -56,12 +57,34 @@ public class RecipeServiceImplTest {
         HashSet recipesData = new HashSet();
         recipesData.add(recipe);
 
-        when(recipeService.getRecipes()).thenReturn(recipesData);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
         assertEquals(recipes.size(), 1);
 
         verify(recipeRepository, times(1)).findAll();
         verify(recipeRepository, never()).findById(anyLong());
+    }
+
+    @Test
+    public void getRecipesCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+
     }
 }
