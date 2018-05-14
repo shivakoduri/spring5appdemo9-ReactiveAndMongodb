475c64ec5d76a19167e39cc0e3b53904a2b60d9e
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-14 22:31:08 +0530

    Adding Spring MVC Exception Handler

--- src/main/java/com/myprojects/spring5/examples/controller/RecipeController.java
+++ src/main/java/com/myprojects/spring5/examples/controller/RecipeController.java
@@ -1,11 +1,14 @@
 package com.myprojects.spring5.examples.controller;
 
 import com.myprojects.spring5.examples.commands.RecipeCommand;
+import com.myprojects.spring5.examples.exceptions.NotFoundException;
 import com.myprojects.spring5.examples.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -51,4 +54,15 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView  modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return  modelAndView;
+    }
+
+
 }
