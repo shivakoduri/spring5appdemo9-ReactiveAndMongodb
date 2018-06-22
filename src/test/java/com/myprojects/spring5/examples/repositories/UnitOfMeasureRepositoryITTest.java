f2ac5ff79fb743e3602df3200ef310135ccf5e9b
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-06-22 22:46:40 +0530

    commit for Integration Testing with MongoDB

--- src/test/java/com/myprojects/spring5/examples/repositories/UnitOfMeasureRepositoryITTest.java
+++ src/test/java/com/myprojects/spring5/examples/repositories/UnitOfMeasureRepositoryITTest.java
@@ -1,11 +1,13 @@
 package com.myprojects.spring5.examples.repositories;
 
+import com.myprojects.spring5.examples.bootstrap.RecipeBootStrap;
 import com.myprojects.spring5.examples.domain.UnitOfMeasure;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,16 +15,28 @@ import java.util.Optional;
 
 import static org.junit.Assert.*;
 
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryITTest {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
     @Before
     public void setUp() throws Exception {
+
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+        recipeRepository.deleteAll();
+
+        RecipeBootStrap recipeBootStrap = new RecipeBootStrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+        recipeBootStrap.onApplicationEvent(null);
     }
 
     @Test
